04e54c8c8b1e10255b951038f72cf86e92625dad
Sneha Akhil Deshmukh <borkar3232@gmail.com>
2025-12-14 19:51:09 +0530

    Adding another log to confirm

--- Ecommarce/src/App.jsx
+++ Ecommarce/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Home from "../Screen/Home";
 import Pdp from "../Screen/Pdp";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
@@ -6,6 +6,10 @@ import Cart from "../Screen/Cart";
 import { basename } from "./Store";
 
 const App = () => {
+
+  useEffect(() => {
+    console.log("App build with basename", basename);
+  }, [])
 
   return <div>
     <Routes basename={basename}>
